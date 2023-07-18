50cc4eea3650dc94d79b40aecf562ca5f314d079
C - 2 <C - 2@WIN-81H12M27IFP>
2023-07-18 21:40:24 +0500

    Добавьте файлы проекта.

---
+++ OriginalFormDemo/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OriginalFormDemo
+{
+    public partial class Form1 : Form
+    {
+        Point pointStart; // точка для перемещения
+        Button buttonClose;
+
+
+        private void addCloseButton() 
+        {
+            // Кнопка закрытия
+            buttonClose = new Button();
+            buttonClose.Location = new Point(this.Width / 3, this.Height / 3);
+            buttonClose.Text = "Х";
+            buttonClose.Click += buttonClose_Click;
+            this.Controls.Add(buttonClose); 
+        }
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void addChoiceForm()
+        {
+            ComboBox cmbChoice = new ComboBox();
+            cmbChoice.Location = new Point(this.Width/3, this.Height/3 + 30);
+            var items = new object[] { "normal", "ellipse", "polygon" };
+            cmbChoice.Items.AddRange(items);
+            cmbChoice.SelectedValueChanged += CmbChoice_SelectedValueChanged;
+            this.Controls.Add(cmbChoice);
+        }
+
+        private void CmbChoice_SelectedValueChanged(object sender, EventArgs e)
+        {
+            OrigWindow.setOriginalForm(this, ((ComboBox)sender).SelectedItem.ToString());
+            OrigWindow.setOriginalButton(this.buttonClose, this.Size, ((ComboBox)sender).SelectedItem.ToString());
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            addCloseButton();
+            addChoiceForm();
+            this.MouseDown += Form1_MouseDown;
+            this.MouseMove += Form1_MouseMove;
+            
+        }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // если нажата левая кнопка мыши
+            if (e.Button == MouseButtons.Left)
+            {
+                pointStart = new Point(e.X, e.Y);
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            // если нажата левая кнопка мыши
+            if ((e.Button & MouseButtons.Left) != 0)
+            {
+                // получаем новую точку положения формы
+                Point deltaPos = new Point(e.X - pointStart.X, e.Y - pointStart.Y);
+                // устанавливаем положение формы
+                this.Location = new Point(this.Location.X + deltaPos.X,
+                  this.Location.Y + deltaPos.Y);
+            }
+        }
+
+        
+        
+    }
+}
---
+++ OriginalFormDemo/OrigWindow.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OriginalFormDemo
+{
+    internal class OrigWindow
+    {
+        public static Region myRegion;
+        static public void setOriginalButton(Button but,Size sizeF, string _form = "normal") {
+
+            System.Drawing.Drawing2D.GraphicsPath myPathButt = new System.Drawing.Drawing2D.GraphicsPath();
+            if (_form == "polygon")
+            {
+                // Выключаем элементы управления формой
+                but.BackColor = Color.White;
+                // Формируем вершины
+                /* Point[] myPoint = { new Point(0,0),
+                 new Point(form.Width, 0),
+                 new Point(form.Width-50, form.Height),
+                 new Point(50, form.Height)};*/
+                myPathButt.AddPolygon(new Point[] { new Point(but.Size.Height, 0), 
+                                                new Point(but.Size.Width/2, 0), 
+                                                new Point(but.Size.Width/2+but.Size.Height, but.Size.Height/3),
+                                                new Point(but.Size.Width/2+but.Size.Height, but.Size.Height-but.Size.Height/3),
+                                                new Point(but.Size.Width/2, but.Size.Height),
+                                                new Point(but.Size.Height, but.Size.Height),
+                                                new Point(0, but.Size.Height-but.Size.Height/3),
+                                                new Point(0, but.Size.Height/3) }); 
+                //myPath.AddEllipse(0, 0, form.Width, form.Height);
+                /*myPath.AddPolygon(myPoint);*/
+                // Устанавливаем видимую область                           
+                myRegion = new Region(myPathButt);
+                but.Region = myRegion;
+            }
+            else
+            {
+                if (_form == "ellipse")
+                {
+                    // Выключаем элементы управления формой
+                    but.BackColor = Color.White;
+                    // Эллипс с высотой и шириной формы
+                    myPathButt.AddEllipse(24, 0, but.Width/3, but.Height);
+                    // Устанавливаем видимую область                           
+                    myRegion = new Region(myPathButt);
+                   but.Region = myRegion;
+
+                }
+                else
+                {                   
+                   
+                    myPathButt.AddPolygon(new Point[] { new Point(0, 0), 
+                                                new Point(but.Size.Width, 0),
+                                                new Point(but.Size.Width, but.Size.Height),
+                                                new Point(0, but.Size.Height) });
+                    myRegion = new Region(myPathButt);
+                    but.Region= myRegion;
+                }
+            }
+        }
+        static public void setOriginalForm(Form form, string _form = "normal")
+        {
+            // Создаём форму границ - объект GraphicsPath
+            System.Drawing.Drawing2D.GraphicsPath myPath = new System.Drawing.Drawing2D.GraphicsPath();
+            if (_form == "polygon")
+            {
+                // Выключаем элементы управления формой
+                form.FormBorderStyle = FormBorderStyle.None;
+                form.BackColor = Color.Orange;
+                // Формируем вершины
+                /* Point[] myPoint = { new Point(0,0),
+                 new Point(form.Width, 0),
+                 new Point(form.Width-50, form.Height),
+                 new Point(50, form.Height)};*/
+                myPath.AddPolygon(new Point[] { new Point(form.Width/4, 0), //Верхняя левая точка
+                                                new Point(form.Width-form.Width/4, 0), //Верхняя правая точка
+                                                new Point(form.Width, form.Height/4),
+                                                new Point(form.Width, form.Height-form.Height/4),
+                                                new Point(form.Width-form.Width/4, form.Height),
+                                                new Point(form.Width/4, form.Height),
+                                                new Point(0, form.Height-form.Height/4),//Нижняя левая точка
+                                                new Point(0, form.Height/4) }); //Нижняя правая точка
+                //myPath.AddEllipse(0, 0, form.Width, form.Height);
+                /*myPath.AddPolygon(myPoint);*/
+                // Устанавливаем видимую область                           
+                myRegion = new Region(myPath);
+                form.Region = myRegion;
+            }
+            else
+            {
+                if (_form == "ellipse")
+                {
+                    // Выключаем элементы управления формой
+                    form.FormBorderStyle = FormBorderStyle.None;
+                    form.BackColor = Color.Orange;
+                    // Эллипс с высотой и шириной формы
+                    myPath.AddEllipse(0, 0, form.Width, form.Height);
+                    // Устанавливаем видимую область                           
+                    myRegion = new Region(myPath);
+                    form.Region = myRegion;
+                    
+                }
+                else
+                {
+                    form.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+                    form.BackColor = Form.DefaultBackColor;
+                }
+            }
+        }
+    }
+}
